5bc97a151472e2b6481755ebfa84a2b1bee80fe2
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-09 17:46:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
 def reverse_each_word (sentence)
   l=Array.new
   l=sentence.split(" ")
+  puts "sentence"
   l.each do |w|
     w.reverse
   end
+  l.inspect
   puts l.join(" ")
 end
 reverse_each_word("Hello my name?")
