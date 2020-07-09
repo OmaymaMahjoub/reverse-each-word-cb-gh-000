a31099ae2098646c68414f0da70f05a0dde8f75a
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-09 17:54:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,10 @@ def reverse_each_word (sentence)
   l=Array.new
   l=sentence.split(" ")
   l2=Array.new
-  l.each do |w|
+  l.collect do |w|
     w1=w.reverse
     l2.push(w1)
   end
   return l2.join(" ")
 end
+
